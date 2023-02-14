72b86347cef21f72c5c4ffc3c7a2dc3c88f12cc1
Dpay <payerdom@my.uri.edu>
2023-02-14 15:00:41 -0500

    Create AddCustomerPage

---
+++ src/main/java/dp/wgu/softwareii/controller/AddCustomerPageController.java
@@ -0,0 +1,40 @@
+package dp.wgu.softwareii.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddCustomerPageController {
+    @javafx.fxml.FXML
+    private Label partTypeLabel;
+    @javafx.fxml.FXML
+    private TextField nameField;
+    @javafx.fxml.FXML
+    private TextField addressField;
+    @javafx.fxml.FXML
+    private TextField postalField;
+    @javafx.fxml.FXML
+    private TextField phoneField;
+    @javafx.fxml.FXML
+    private Label partTypeLabel1;
+    @javafx.fxml.FXML
+    private ComboBox countryComboBox;
+    @javafx.fxml.FXML
+    private ComboBox stateComboBox;
+    @javafx.fxml.FXML
+    private Button AddButton;
+    @javafx.fxml.FXML
+    private Button CancelBtn;
+
+    @javafx.fxml.FXML
+    public void OnSaveClick(ActionEvent actionEvent) {
+        // TODO: save customer and return to Dashboard
+    }
+
+    @javafx.fxml.FXML
+    public void OnCancelClick(ActionEvent actionEvent) {
+        // TODO: return to DashBoard
+    }
+}
